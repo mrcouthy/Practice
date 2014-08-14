c8cc20d07fa9da91e810e497890ddeecdfffe7c4
Dhiraj B <mrdhiraj@gmail.com>
2014-08-14 11:09:24 +0545

    First Working Model of KnockOut js , place kno.js content at end of body load ..finding it killed some of my time.

---
+++ KnockOutPractice/kno.js
@@ -0,0 +1,16 @@
+function AppViewModel() {
+    this.firstName = ko.observable("Bdert");
+    this.lastName = ko.observable("Bertington");
+    
+    this.fullName = ko.computed(function(){
+    return this.firstName() + " " + this.lastName();
+    },this);
+    
+    this.capitalizeLastName= function(){
+    var currentVal = this.lastName();
+    this.lastName(currentVal.toUpperCase());
+    };
+}
+
+// Activates knockout.js
+ko.applyBindings(new AppViewModel());
