a8a955c960e1ff3b14c1f77a771bb6520e2ccead
mrcouthy <mrcouthy@gmail.com>
2014-08-25 19:10:21 +0545

    Adding Moq practices

---
+++ MoqPractice/MoqPractice/Program.cs
@@ -0,0 +1,33 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoqPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var mock = new Mock<IFoo>();
+            mock.Setup(foo => foo.DoSomething("ping")).Returns(true);
+            IFoo  f = mock.Object;
+            bool b = f.DoSomething("");
+            bool c = f.DoSomething("ping");
+        }
+
+
+       
+    }
+
+    class Foo : IFoo
+    {
+        public bool DoSomething(string p)
+        {
+            return true;
+        }
+    }
+
+}
