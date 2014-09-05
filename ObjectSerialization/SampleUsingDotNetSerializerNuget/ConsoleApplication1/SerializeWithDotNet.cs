9f07cd40bf411b2c6939a1d152921712632fcdbd
mrcouthy <mrcouthy@gmail.com>
2014-09-05 18:09:31 +0545

    Object serialization

---
+++ ObjectSerialization/SampleUsingDotNetSerializerNuget/ConsoleApplication1/SerializeWithDotNet.cs
@@ -0,0 +1,48 @@
+﻿using System.IO;
+using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace JDE.Common.Helper
+{
+    public class XmlSerialization<T> where T : class
+    {
+        /// <summary>
+        /// Gives Object from xml String
+        /// </summary>
+        /// <param name="xml"></param>
+        /// <returns></returns>
+        public static T GetObject(string xml)
+        {
+            T result;
+            byte[] byteArray = Encoding.UTF8.GetBytes(xml);
+
+            using (MemoryStream stream = new MemoryStream(byteArray))
+            {
+                XmlSerializer serializer = new XmlSerializer(typeof(T));
+                result = (T)serializer.Deserialize(stream);
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Gives Xml string of any serializable object
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public static string GetXml<T>(T obj)
+        {
+            string xmlString = string.Empty;
+            using (var stream = new MemoryStream())
+            {
+                using (var writer = XmlWriter.Create(stream))
+                {
+                    new XmlSerializer(obj.GetType()).Serialize(writer, obj);
+                    xmlString = Encoding.UTF8.GetString(stream.ToArray());
+                }
+            }
+            return xmlString;
+        }
+    }
+}
---
+++ ObjectSerialization/SampleUsingDotNetSerializerNuget/ConsoleApplication1/Test.cs
@@ -0,0 +1,26 @@
+﻿using JDE.Common.Tests;
+using KellermanSoftware.CompareNetObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XSerializer;
+
+namespace ConsoleApplication1
+{
+    public class Test
+    {
+        public static void meth()
+        {
+            var v = DataGenerator.GetUserDataSets();
+            XmlSerializer<ReportModel> s = new XmlSerializer<ReportModel>();
+            string xml = s.Serialize(v);
+            ReportModel rm = s.Deserialize(xml);
+            rm.Name = "a";
+            CompareLogic compareLogic = new CompareLogic();
+            ComparisonResult result = compareLogic.Compare(v, rm);
+            bool b =result.AreEqual;
+        }
+    }
+}
