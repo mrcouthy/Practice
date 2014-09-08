0133ef59b021cd56ae77c09e0d60c7815bdfbb92
mrcouthy <mrcouthy@gmail.com>
2014-09-08 21:02:39 +0545

    fadeVisible is a custom binding

--- KnockOutPractice/CustomBiniding/knoLists.js
+++ KnockOutPractice/CustomBiniding/knoLists.js
@@ -1,4 +1,34 @@
 $(document).ready(function () {
+    // ----------------------------------------------------------------------------
+    // Reusable bindings - ideally kept in a separate file
+
+    ko.bindingHandlers.fadeVisible = {
+        init: function (element, valueAccessor) {
+            // Start visible/invisible according to initial value
+            var shouldDisplay = valueAccessor();
+            $(element).toggle(shouldDisplay);
+        },
+        update: function (element, valueAccessor) {
+            // On update, fade in/out
+            var shouldDisplay = valueAccessor();
+            shouldDisplay ? $(element).fadeIn() : $(element).fadeOut();
+        }
+    };
+
+    ko.bindingHandlers.jqButton = {
+        init: function (element) {
+            $(element).button(); // Turns the element into a jQuery UI button
+        },
+        update: function (element, valueAccessor) {
+            var currentValue = valueAccessor();
+            // Here we just update the "disabled" state, but you could update other properties too
+            $(element).button("option", "disabled", currentValue.enable === false);
+        }
+    };
+
+    // ----------------------------------------------------------------------------
+    // Page viewmodel
+
     function Answer(text) { this.answerText = text; this.points = ko.observable(1); }
 
     function SurveyViewModel(question, pointsBudget, answers) {
