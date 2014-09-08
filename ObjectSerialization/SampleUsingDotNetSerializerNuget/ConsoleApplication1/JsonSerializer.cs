4331bd05fa4bc52b8fec93a47ffec916d173796a
mrcouthy <mrcouthy@gmail.com>
2014-09-08 14:36:38 +0545

    adding hot chai and json serialization

---
+++ ObjectSerialization/SampleUsingDotNetSerializerNuget/ConsoleApplication1/JsonSerializer.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    [DataContract]
+    class Spell
+    {
+        [DataMember]
+        public String cast;
+
+        [DataMember]
+        public String cooldown;
+
+        [DataMember]
+        public String cost;
+
+        [DataMember]
+        public String description;
+
+        [DataMember]
+        public String icon;
+
+        [DataMember]
+        public Int16 id;
+
+        [DataMember]
+        public String name;
+
+        [DataMember]
+        public String range;
+    }
+
+    public class JsonSerialization<T> where T : class
+    {
+        /// <summary>
+        /// Gives Xml string of any serializable object
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public static string GetString(T obj)
+        {
+            DataContractJsonSerializer jSerializer = new DataContractJsonSerializer(typeof(T));
+            MemoryStream memoryStream = new MemoryStream();
+            jSerializer.WriteObject(memoryStream, obj);
+            memoryStream.Position = 0;
+            StreamReader streamReader = new StreamReader(memoryStream);
+            string json = streamReader.ReadToEnd();
+            streamReader.Close();
+            memoryStream.Close();
+            return json;
+        }
+        /// <summary>
+        /// Gives Object from xml String
+        /// </summary>
+        /// <param name="plainString"></param>
+        /// <returns></returns>
+        public static T GetObject(string plainString)
+        {
+            DataContractJsonSerializer jSerializer = new DataContractJsonSerializer(typeof(T));
+            MemoryStream memoryStream = new MemoryStream(System.Text.ASCIIEncoding.ASCII.GetBytes(plainString));
+            T obj = (T)jSerializer.ReadObject(memoryStream);
+            memoryStream.Close();
+            return obj;
+        }
+
+
+    }
+}
--- ObjectSerialization/SampleUsingDotNetSerializerNuget/ConsoleApplication1/Program.cs
+++ ObjectSerialization/SampleUsingDotNetSerializerNuget/ConsoleApplication1/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using JDE.Common.Tests;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,7 +12,17 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             //Test.meth();
-            Test.methDotNet();
+            //Test.methDotNet();
+            String spellData = "{ \"id\": 133, \"name\": \"Fireball\", \"icon\": \"spell_fire_flamebolt\", \"description\": \"Hurls a fiery ball that causes 1,846 Fire damage.\", \"range\": \"40 yd range\", \"powerCost\": \"4% of base mana\", \"castTime\": \"2.25 sec cast\"}";
+            Spell s = JsonSerialization<Spell>.GetObject(spellData);
+
+            string newdata = JsonSerialization<Spell>.GetString(s);
+
+            var v = DataGenerator.GetUserDataSets();
+            
+            string json = JsonSerialization<ReportModel>.GetString(v);
+            ReportModel rm = JsonSerialization<ReportModel>.GetObject(json);
+
         }
     }
 }
