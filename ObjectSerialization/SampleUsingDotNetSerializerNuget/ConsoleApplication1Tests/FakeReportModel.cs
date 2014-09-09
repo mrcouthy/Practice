f36ba6ab6fe4b6011da91d02e652b6aa2ee00e80
mrcouthy <mrcouthy@gmail.com>
2014-09-09 19:32:58 +0545

    add tests

---
+++ ObjectSerialization/SampleUsingDotNetSerializerNuget/ConsoleApplication1Tests/FakeReportModel.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JDE.Common.Tests
+{
+
+    [Serializable]
+    public class ReportModel
+    {
+        public string Name { get; set; }
+        public List<UserDataSetModel> MyDataSets { get; set; }
+    }
+
+  public class UserTable
+    {
+        public string Name { get; set; }
+        public List<ColumnDefinition> Columns { get; set; }
+    }
+
+    public class ColumnDefinition
+    {
+        public string Name { get; set; }
+    }
+
+    public class UserDataSetModel
+    {
+        public string Name { get; set; }
+        public string Server { get; set; }
+        public List<UserTable> Tables { get; set; }
+    }
+
+    public class DataGenerator
+    {
+        public static ReportModel GetUserDataSets()
+        {
+            var policy = new UserDataSetModel
+            {
+                Name = "Policy",
+                Tables =
+                    new List<UserTable>
+                    {
+                        new UserTable
+                        {
+                            Name = "Exposure",
+                            Columns =
+                                new List<ColumnDefinition>
+                                {
+                                    new ColumnDefinition { Name = "ExosureId" }
+                                   
+                                }
+                        } 
+                       
+                    }
+            };
+
+            var claims = new UserDataSetModel
+            {
+                Name = "Claims",
+                Tables =
+                    new List<UserTable>
+                    {
+                        new UserTable
+                        {
+                            Name = "BenefitType",
+                            Columns =
+                                new List<ColumnDefinition>
+                                {
+                                    new ColumnDefinition { Name = "BenefitTypeId" },
+                                    new ColumnDefinition { Name = "BenefitType" },
+                                    new ColumnDefinition { Name = "BenefitTypeLevel" },
+                                }
+                        },
+                        new UserTable
+                        {
+                            Name = "Benefit Table 2",
+                            Columns =
+                                new List<ColumnDefinition>
+                                {
+                                    new ColumnDefinition { Name = "BenefitType2Id" },
+                                    new ColumnDefinition { Name = "BenefitType2" },
+                                    new ColumnDefinition { Name = "BenefitTypeLevel1" },
+                                }
+                        }
+                    }
+            };
+
+            var expenses = new UserDataSetModel
+            {
+                Name = "Expenses",
+                Tables =
+                    new List<UserTable>
+                    {
+                        new UserTable
+                        {
+                            Name = "ExpenseType",
+                            Columns =
+                                new List<ColumnDefinition>
+                                {
+                                    new ColumnDefinition { Name = "ExpenseTypeId" },
+                                    new ColumnDefinition { Name = "ExpenseType" },
+                                    new ColumnDefinition { Name = "ExpenseTypeLevel" },
+                                }
+                        },
+                        new UserTable
+                        {
+                            Name = "Expenses Table 2",
+                            Columns =
+                                new List<ColumnDefinition>
+                                {
+                                    new ColumnDefinition { Name = "ExpenseType2Id" },
+                                    new ColumnDefinition { Name = "ExpenseType2" },
+                                    new ColumnDefinition { Name = "ExpenseType3" },
+                                }
+                        }
+                    }
+            };
+
+            var datasets = new List<UserDataSetModel> { policy };
+            ReportModel reportModel = new ReportModel();
+            reportModel.MyDataSets = datasets;
+            return reportModel;
+        }
+    }
+}
+
---
+++ ObjectSerialization/SampleUsingDotNetSerializerNuget/ConsoleApplication1Tests/ObjectSerializationTest.cs
@@ -0,0 +1,47 @@
+﻿//using JDE.Common.Helper;
+//using KellermanSoftware.CompareNetObjects;
+//using NUnit.Framework;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace JDE.Common.Tests
+//{
+//    class ObjectSerializationTest
+//    {
+//        [Test]
+//        public void TestIfSerializationAndDesirializationOfIntegerListAreEqual()
+//        {
+//            var inputList = new List<int>() { 1, 2, 3 };
+//            string outputXML = JsonSerialization<List<int>>.GetString(inputList);
+//            var outPutList = JsonSerialization<List<int>>.GetObject(outputXML);
+//            Assert.AreEqual(inputList, outPutList);
+//        }
+
+//        [Test]
+//        public void TestIfSerializationAndDesrializationOfReportModelAreEqual()
+//        {
+//            var reportModel = DataGenerator.GetUserDataSets();
+//            string outputXML = JsonSerialization<ReportModel>.GetString(reportModel);
+//            var outputReportModel = JsonSerialization<ReportModel>.GetObject(outputXML);
+//            CompareLogic compareLogic = new CompareLogic();
+//            ComparisonResult result = compareLogic.Compare(reportModel, outputReportModel);
+//         //  result.Differences 
+//            Assert.That(result.AreEqual,Is.EqualTo (true));
+//        }
+
+//        [Test]
+//        public void TestIfSerializationAndDesrializationOfReportModelAreNotEqual()
+//        {
+//            var reportModel = DataGenerator.GetUserDataSets();
+//            string outputXML = JsonSerialization<ReportModel>.GetString(reportModel);
+//            var outputReportModel = JsonSerialization<ReportModel>.GetObject(outputXML);
+//            outputReportModel.Name = "NewName";
+//            CompareLogic compareLogic = new CompareLogic();
+//            ComparisonResult result = compareLogic.Compare(reportModel, outputReportModel);
+//            Assert.That(result.AreEqual, Is.EqualTo(false));
+//        }
+//    }
+//}
