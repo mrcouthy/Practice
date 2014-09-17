ded51e6c054c32a488fa1747301464528f3ca9e3
mrcouthy <mrcouthy@gmail.com>
2014-09-17 11:00:59 +0545

    Type of

--- JavaScript/Simple JS/js.js
+++ JavaScript/Simple JS/js.js
@@ -5,6 +5,13 @@ window.onload = function () {
     //Mulitline text
     console.log('test\
  log');
-    alert(obj.name);
+    console.log(typeof "Str");
+    console.log(typeof 123);
+    console.log(typeof false);
+    console.log(typeof [1, 2, 3]);
+    console.log(typeof obj);
+    console.log(typeof {});
+    console.log(typeof {name:"ram"});
+
 }
 
