8be8e8ecc2ead1434b14838ccebbe7e14ac35c56
mrcouthy <mrcouthy@gmail.com>
2014-09-18 13:00:18 +0545

    Get all arguments passed to a function

--- JavaScript/Mid level Js/functions.js
+++ JavaScript/Mid level Js/functions.js
@@ -1,8 +1,31 @@
 window.onload = function () {
+   // defineFunction();
+    //console.log(defineFunction.toString());//prints the function
+    argumentObjectSample();
+   
+
+}
+
+function argumentObjectSample()
+{
+    var x = findsum(1, 123, 500, 115, 44, 88);
+    //Like variabes functions are also Hoisted ie you can call function before declaring it
+    function findsum() {
+        var i = 0,sum=0;
+        for (i = 0; i < arguments.length; i++)
+        {
+            sum = sum + arguments[i];
+        }
+        return sum;
+    }
+    console.log(x);
+}
+
+function defineFunction()
+{
     //create functions
-    function add(a,b)
-    {
-        return a+b;
+    function add(a, b) {
+        return a + b;
     }
 
     //annonymous function
@@ -11,11 +34,9 @@ window.onload = function () {
     //function constructor
     var substract = new Function("a", "b", "return a-b");
 
-    console.log(add(1,2));
+    console.log(add(1, 2));
     console.log(multiply(1, 2));
-
-
     console.log(substract(1, 2));
-
 }
- 
+
+
