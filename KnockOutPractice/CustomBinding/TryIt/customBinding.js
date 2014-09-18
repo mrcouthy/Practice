54bd03d8ccd66b5a74c471327d2c0254af477a00
mrcouthy <mrcouthy@gmail.com>
2014-09-18 17:30:25 +0545

    Simplify and add some info

--- KnockOutPractice/CustomBinding/TryIt/customBinding.js
+++ KnockOutPractice/CustomBinding/TryIt/customBinding.js
@@ -4,30 +4,23 @@ $(document).ready(function () {
     };
 
     ko.bindingHandlers.slideVisible = {
-        init: function (element, valueAccessor) {
+        init: function (element, valueAccessor) { //valueAccessor is actually a function 
             var value = ko.unwrap(valueAccessor()); // Get the current value of the current property we're bound to
             $(element).toggle(value); // jQuery will hide/show the element depending on whether "value" or true or false
-            console.log(value);
         },
         update: function (element, valueAccessor, allBindings) {
-            var value = valueAccessor();
-            console.log(value);
+            var value = ko.unwrap(valueAccessor());
 
-            // Next, whether or not the supplied model property is observable, get its current value
-            var valueUnwrapped = ko.unwrap(value);
-            console.log(valueUnwrapped);
             // Grab some more data from another binding property
             var duration = allBindings.get('slideDuration') || 400; // 400ms is default duration unless otherwise specified
 
-            // Now manipulate the DOM element
-            if (valueUnwrapped == true)
-                $(element).slideDown(duration); // Make the element visible
+            // Now manipulate the DOM element using jQuery
+            if (value)
+                $(element).slideDown(duration);
             else
-                $(element).slideUp(duration);   // Make the element invisible
+                $(element).slideUp(duration);
         }
     }
-
-
 
     ko.applyBindings(viewModel);
 }
