4f9bf6bf6291b57c4aaeeeb5ff084d2ee4a10df3
mrcouthy <mrcouthy@gmail.com>
2014-09-19 17:40:33 +0545

    Making it simple

---
+++ Log4NetPractice/Logging/Logging/Log4NetFromConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using log4net;
+using log4net.Config;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logging
+{
+    public static class Log4NetFromConfiguration
+    {
+        public static void Test()
+        {
+            ILog Log = LogManager.GetLogger("test");
+            XmlConfigurator.Configure(); //only once
+            Log.Debug("Application is starting");
+            Console.WriteLine("Test Line");
+            Log.Debug("Application is ending");
+            Console.Read();
+        }
+    }
+}
--- Log4NetPractice/Logging/Logging/Program.cs
+++ Log4NetPractice/Logging/Logging/Program.cs
@@ -11,28 +11,12 @@ namespace Logging
 {
     class Program
     {
-        private static readonly ILog Log = LogManager.GetLogger("test");
         static void Main(string[] args)
         {
-            XmlConfigurator.Configure(); //only once
-            Log.Debug("Application is starting");
-            Console.WriteLine("Test Line");
-            var testClass = new TestClass();
-            testClass.LogSomething();
-            Log.Debug("Application is ending");
-            Console.Read();
+            Log4NetFromConfiguration.Test();
+
         }
     }
 
-    public class TestClass
-    {
-        private static readonly ILog Log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
-        public void LogSomething()
-        {
-            for (int i = 0; i < 100; i++)
-            {
-                Log.InfoFormat("CurrentTime is [{0}]", DateTime.Now.ToString("yyyy.MM.dd-hh.mm.ss~fff"));
-            }
-        }
-    }
+   
 }
