9f0a60a22c8a5a4467d3c6afa4007d7de4b56c33
mrcouthy <mrcouthy@gmail.com>
2014-09-19 17:46:35 +0545

    adding configure by code

--- Log4NetPractice/Logging/Logging/Log4NetConfigureByCode.cs
+++ Log4NetPractice/Logging/Logging/Log4NetConfigureByCode.cs
@@ -1,5 +1,9 @@
 ﻿using log4net;
+using log4net.Appender;
 using log4net.Config;
+using log4net.Core;
+using log4net.Layout;
+using log4net.Repository.Hierarchy;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,14 +16,43 @@ namespace Logging
     {
         public static void Test()
         {
+            Setup();
             ILog Log = LogManager.GetLogger("test");
-            XmlConfigurator.Configure(); //only once
             Log.Debug("Application is starting");
-            Console.WriteLine("Test Line");
             Log.Debug("Application is ending");
             Console.Read();
         }
+
+        public static void Setup()
+        {
+            Hierarchy hierarchy = (Hierarchy)LogManager.GetRepository();
+
+            PatternLayout patternLayout = new PatternLayout();
+            patternLayout.ConversionPattern = "%date [%thread] %-5level %logger - %message%newline";
+            patternLayout.ActivateOptions();
+
+            RollingFileAppender roller = new RollingFileAppender();
+            roller.AppendToFile = false;
+            roller.File = @"Logs\EventLog.txt";
+            roller.Layout = patternLayout;
+            roller.MaxSizeRollBackups = 5;
+            roller.MaximumFileSize = "1GB";
+            roller.RollingStyle = RollingFileAppender.RollingMode.Size;
+            roller.StaticLogFileName = true;
+            roller.ActivateOptions();
+            hierarchy.Root.AddAppender(roller);
+
+            MemoryAppender memory = new MemoryAppender();
+            memory.ActivateOptions();
+            hierarchy.Root.AddAppender(memory);
+
+            hierarchy.Root.Level = Level.Debug;
+            hierarchy.Configured = true;
+        }
+
     }
 
- 
+
+
+
 }
