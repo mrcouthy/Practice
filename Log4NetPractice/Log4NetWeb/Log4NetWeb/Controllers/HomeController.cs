0a8c06800f4ff289d812870ad9fcf8a86916b218
Dhiraj B <mrdhiraj@gmail.com>
2014-09-22 07:52:50 +0545

    First sample complete

---
+++ Log4NetPractice/Log4NetWeb/Log4NetWeb/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Log4NetWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public void Index()
+        {
+            try
+            {
+                throw new Exception("Hi !");
+            }
+            catch (Exception ex)
+            {
+                Logger.Error(ex);
+            }
+            
+        }
+
+    }
+}
