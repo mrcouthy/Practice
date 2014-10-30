e4aba1808864c0b3ccc0332d536dc3a88abd8aea
Dhiraj <mrdhiraj@gmail.com>
2014-10-30 10:59:31 +0545

    simpler

--- KnockOutPractice/Simple/simple.js
+++ KnockOutPractice/Simple/simple.js
@@ -1,8 +1,7 @@
 $(function () {
 // Here's my data model
 var ViewModel = function() {
-    this.firstName = ko.observable('');
-    this.lastName = ko.observable('');
+   this.name = ko.observable('');
 };
  
 ko.applyBindings(new ViewModel()); // This makes Knockout get to work
