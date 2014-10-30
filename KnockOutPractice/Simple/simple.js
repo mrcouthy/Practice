c1ec5394d0c19c05ff49cf38818e3afb9b7feabc
Dhiraj <mrdhiraj@gmail.com>
2014-10-30 11:45:25 +0545

    Adding arrays

--- KnockOutPractice/Simple/simple.js
+++ KnockOutPractice/Simple/simple.js
@@ -1,18 +1,23 @@
 $(function () {
 // Here's my data model
-var ViewModel = function(firstName,lastName) {
-    this.firstName = ko.observable(firstName);
-    this.lastName = ko.observable(lastName);
-    this.fullName = ko.computed(function () {
+    var ViewModel = function (firstName, lastName,phones) {
+     var   self = this;
+     self.firstName = ko.observable(firstName);
+     self.lastName = ko.observable(lastName);
+     self.fullName = ko.computed(function () {
         return this.firstName() + ' ' + this.lastName();
     }, this);//this is requrired because function does not belong to any object initially
 
-    this.fullName2 = ko.computed(function () {
+    self.fullName2 = ko.computed(function () {
         return this.lastName() + ' ' + this.firstName();
-    }, this);
+    }, self);
+
+    self.phones = ko.observableArray(phones);
+   
+    
 };
 
-ko.applyBindings(new ViewModel('Dhiraj','Bajracharya')); // This makes Knockout get to work
+    ko.applyBindings(new ViewModel('Dhiraj', 'Bajracharya', ['9841190907', '5536114'])); // This makes Knockout get to work
 });
 
 
