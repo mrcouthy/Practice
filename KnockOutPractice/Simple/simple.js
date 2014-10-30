2c670443035e5ae93b13fad5718bbbfce4c38547
Dhiraj <mrdhiraj@gmail.com>
2014-10-30 11:31:47 +0545

    adding computed

--- KnockOutPractice/Simple/simple.js
+++ KnockOutPractice/Simple/simple.js
@@ -1,10 +1,14 @@
 $(function () {
 // Here's my data model
-var ViewModel = function(name) {
-   this.name = ko.observable(name);
+var ViewModel = function(firstName,lastName) {
+    this.firstName = ko.observable(firstName);
+    this.lastName = ko.observable(lastName);
+    this.fullName = ko.computed(function () {
+        return this.firstName() + this.lastName();
+    },this)//this is requrired because function does not belong to any object initially
 };
- 
-ko.applyBindings(new ViewModel('Dhiraj')); // This makes Knockout get to work
+
+ko.applyBindings(new ViewModel('Dhiraj','Bajracharya')); // This makes Knockout get to work
 });
 
 
