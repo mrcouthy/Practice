12ea15e8167529ce20f88f4522fe837704e5f648
Dhiraj <mrdhiraj@gmail.com>
2014-11-02 10:11:03 +0545

    clean up

--- KnockOutPractice/Simple/simple.js
+++ KnockOutPractice/Simple/simple.js
@@ -1,38 +1,35 @@
 $(function () {
-// Here's my data model
-    var ViewModel = function (firstName, lastName,phones) {
-     var   self = this;
-     self.firstName = ko.observable(firstName);
-     self.lastName = ko.observable(lastName);
-     self.fullName = ko.computed(function () {
-        return this.firstName() + ' ' + this.lastName();
-    }, this);//this is requrired because function does not belong to any object initially
+    // Here's my data model
+    var ViewModel = function (firstName, lastName, phones) {
+        var self = this;
+        self.firstName = ko.observable(firstName);
+        self.lastName = ko.observable(lastName);
+        self.fullName = ko.computed(function () {
+            return this.firstName() + ' ' + this.lastName();
+        }, this);//this is requrired because function does not belong to any object initially
 
-    self.fullName2 = ko.computed(function () {
-        return this.lastName() + ' ' + this.firstName();
-    }, self);
+        self.fullName2 = ko.computed(function () {
+            return this.lastName() + ' ' + this.firstName();
+        }, self);
+        //arrya example
+        self.phones = ko.observableArray(phones);
 
-    self.phones = ko.observableArray(phones);
-   
-    this.newPhone = ko.observable();
+        this.newPhone = ko.observable();
 
-    this.addPhone = function ()
-    {
-        if (this.newPhone()!="") {
-            this.phones.push(this.newPhone());// try this with no brackets ie this.newPhone and get mad !
-            this.newPhone("");
+        this.addPhone = function () {
+            if (this.newPhone() != "") {
+                this.phones.push(this.newPhone());// try this with no brackets ie this.newPhone and get mad !
+                this.newPhone("");
+            }
+            //when using observables always access members as as function ie newphone() => value not noewphone => observable
         }
-       
-        //when using observables always access members as as function ie newphone() => value not noewphone => observable
-    }
 
-    this.selectedPhone = ko.observable("");
-    
-};
+        this.selectedPhone = ko.observable("");
 
+        //more complex select example
+    };
     ko.applyBindings(new ViewModel('Dhiraj', 'Bajracharya', ['9841190907'])); // This makes Knockout get to work
 });
 
 
 
-   
