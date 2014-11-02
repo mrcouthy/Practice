979741a75abb7a7bda2bdc5199ae929786c64237
Dhiraj <mrdhiraj@gmail.com>
2014-11-02 13:14:08 +0545

    on way to cascading dropdown

--- KnockOutPractice/Complex/complex.js
+++ KnockOutPractice/Complex/complex.js
@@ -1,29 +1,37 @@
 $(function () {
     // Here's my data model
-    var ViewModel = function (firstName, lastName, phones) {
+    var ViewModel = function () {
         var self = this;
-        //self.phones = ko.computed(function () {
-        //    return phones;
-        //});
+        var persons = [{ name: "dhiraj", id: "1"  }, { name: "poonam", id: "2"  },
+          { name: "hero", id: "3"  }
+        ];
 
+        self.persons = persons;
+        this.selectedPerson = ko.observable("");
+this.selectedPhone = ko.observable("");
+        var telephones = [{ name: "dhiraj", id: "1", phone: "9841190907" }, { name: "home", id: "1", phone: "5536114" },
+            { name: "poonam", id: "2", phone: "9841228718" }, { name: "home", id: "2", phone: "44151254" },
+            { name: "Hero", id: "3", phone: "984166996699" }, { name: "home", id: "3", phone: "5536101" }
+        ];
         self.phones = ko.computed(function () {
 
-            var filteredPhones = ko.utils.arrayFilter(phones, function (item) {
-                if(item.length>1)
+            var filteredPhones = ko.utils.arrayFilter(telephones, function (item) {
+                
+                if (item.id == this.selectedPerson)
                 return item;
             });
-
             return filteredPhones;
         });
-
-        this.selectedPhone = ko.observable("");
+       // self.phones = telephones;
+        console.log("hello" +  this.selectedPerson());
+        
         //more complex select example
         // var dropDown = [{ name: "dhiraj", id: "1", phone: "9841190907" }, { name: "Hero", id: "2", phone: "5536114" }];
 
     };
 
 
-    ko.applyBindings(new ViewModel('Dhiraj', 'Bajracharya', ['9841190907', '9841228718','a','bb'])); // This makes Knockout get to work
+    ko.applyBindings(new ViewModel()); // This makes Knockout get to work
 });
 
 
