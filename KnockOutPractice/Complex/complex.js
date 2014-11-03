e69ae6c8dea90ece223b15ad660b36b05b55d873
Dhiraj <mrdhiraj@gmail.com>
2014-11-03 11:56:28 +0545

    trying out

--- KnockOutPractice/Complex/complex.js
+++ KnockOutPractice/Complex/complex.js
@@ -8,6 +8,8 @@ $(function () {
 
         self.persons = persons;
         this.selectedPerson = ko.observable("");
+        this.selectedPerson(1);
+        var test = "2";
 this.selectedPhone = ko.observable("");
         var telephones = [{ name: "dhiraj", id: "1", phone: "9841190907" }, { name: "home", id: "1", phone: "5536114" },
             { name: "poonam", id: "2", phone: "9841228718" }, { name: "home", id: "2", phone: "44151254" },
@@ -17,7 +19,7 @@ this.selectedPhone = ko.observable("");
 
             var filteredPhones = ko.utils.arrayFilter(telephones, function (item) {
                 
-                if (item.id == this.selectedPerson)
+                if (item.id == test)
                 return item;
             });
             return filteredPhones;
