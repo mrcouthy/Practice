c8bb720f8a05a43f3396d5055182390138e57f0e
mrcouthy <mrcouthy@gmail.com>
2014-12-26 15:19:23 +0545

    Add files for code snippet

--- HangFire/TryHf/TryHf/Controllers/HomeController.cs
+++ HangFire/TryHf/TryHf/Controllers/HomeController.cs
@@ -1,17 +1,56 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
+using Hangfire.Highlighter.Models;
+using TryHf.Models;
 
-namespace TryHf.Controllers
+namespace Hangfire.Highlighter.Controllers
 {
     public class HomeController : Controller
     {
-        //http://docs.hangfire.io/en/latest/tutorials/highlight.html
-        public string Index()
+        private readonly HighlighterDbContext _db = new HighlighterDbContext();
+
+        public ActionResult Index()
         {
-            return "Hello";
+            return View(_db.CodeSnippets.ToList());
+        }
+
+        public ActionResult Details(int id)
+        {
+            var snippet = _db.CodeSnippets.Find(id);
+            return View(snippet);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create([Bind(Include = "SourceCode")] CodeSnippet snippet)
+        {
+            if (ModelState.IsValid)
+            {
+                snippet.CreatedAt = DateTime.UtcNow;
+
+                // We'll add the highlighting a bit later.
+
+                _db.CodeSnippets.Add(snippet);
+                _db.SaveChanges();
+
+                return RedirectToAction("Details", new { id = snippet.Id });
+            }
+
+            return View(snippet);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _db.Dispose();
+            }
+            base.Dispose(disposing);
         }
     }
 }
