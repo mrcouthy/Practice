a06903452b6b15cdce0422199f83b9a0506f232c
mrcouthy <mrcouthy@gmail.com>
2014-12-26 14:34:57 +0545

    Hangfire Package added and also OWIN startUp for it

---
+++ HangFire/TryHf/TryHf/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using Hangfire.SqlServer;
+using Microsoft.Owin;
+
+[assembly: OwinStartup(typeof(Hangfire.Highlighter.Startup))]
+
+namespace Hangfire.Highlighter
+{
+    using Owin;
+
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseHangfire(
+                config => config.UseSqlServerStorage("TryHf"));
+        }
+      
+    }
+}
