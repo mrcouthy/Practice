bd7bbc12d09311c472a123c062270234e90bab70
Dj <mrcouthy@gmail.com>
2014-12-28 21:24:24 +0545

    adding set up

---
+++ HangFire/SendEmail/SendEmail/Startup.cs
@@ -0,0 +1,21 @@
+﻿using Hangfire;
+using Hangfire.SqlServer;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(MyWebApplication.Startup))]
+
+namespace MyWebApplication
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseHangfire(config =>
+            {
+                config.UseSqlServerStorage("emailDb");
+                config.UseServer();
+            });
+        }
+    }
+}
