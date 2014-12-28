b09c271a5d8a3799512489d73420b0596c213ef6
Dj <mrcouthy@gmail.com>
2014-12-28 20:58:18 +0545

    http://docs.hangfire.io/en/latest/tutorials/send-email.html Install-Package Postal Install-Package Hangfire

---
+++ HangFire/SendEmail/SendEmail/Startup.cs
@@ -0,0 +1,23 @@
+﻿using Hangfire;
+using Hangfire.SqlServer;
+using Microsoft.Owin;
+using Owin;
+
+
+[assembly: OwinStartupAttribute(typeof(SendEmail.Startup))]
+namespace SendEmail
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+
+            app.UseHangfire(config =>
+            {
+                config.UseSqlServerStorage("<name or connection string>");
+                config.UseServer();
+            });
+        }
+    }
+}
