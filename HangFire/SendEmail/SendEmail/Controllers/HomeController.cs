0541d2d134da255d72436e0b2d9b5b572eac9442
Dj <mrcouthy@gmail.com>
2014-12-29 04:59:48 +0545

    more codes

---
+++ HangFire/SendEmail/SendEmail/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using HangFire.Mailer.Models;
+using Hangfire.Mailer.Models;
+
+namespace HangFire.Mailer.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly MailerDbContext _db = new MailerDbContext();
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var comments = _db.Comments.OrderBy(x => x.Id).ToList();
+            return View(comments);
+        }
+
+        [HttpPost]
+        public ActionResult Create(Comment model)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Comments.Add(model);
+                _db.SaveChanges();
+                var email = new NewCommentEmail{
+                    To = "mrcouthy@gmail.com",
+                    UserName =model.UserName ,
+                    Comment = model.Text 
+                };
+                email.Send();
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _db.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+    }
+}
