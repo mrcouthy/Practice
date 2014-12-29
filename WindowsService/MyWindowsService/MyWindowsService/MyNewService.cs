ce6fe62c6496989e30a914662515b9f0e3b74916
mrcouthy <mrcouthy@gmail.com>
2014-12-29 14:02:41 +0545

    Adding windows eventlog component

--- WindowsService/MyWindowsService/MyWindowsService/MyNewService.cs
+++ WindowsService/MyWindowsService/MyWindowsService/MyNewService.cs
@@ -15,14 +15,30 @@ namespace MyWindowsService
         public MyNewService()
         {
             InitializeComponent();
+            //Add or edit the constructor to define a custom event log: 
+            eventLog1 = new System.Diagnostics.EventLog();
+            if (!System.Diagnostics.EventLog.SourceExists("MySource"))
+            {
+                System.Diagnostics.EventLog.CreateEventSource(
+                    "MySource", "MyNewLog");
+            }
+            eventLog1.Source = "MySource";
+            eventLog1.Log = "MyNewLog";
         }
 
         protected override void OnStart(string[] args)
         {
+            eventLog1.WriteEntry("In on start!");
         }
 
         protected override void OnStop()
         {
+            eventLog1.WriteEntry("In onStop.");
+        }
+
+        protected override void OnContinue()
+        {
+            eventLog1.WriteEntry("In OnContinue.");
         }
     }
 }
