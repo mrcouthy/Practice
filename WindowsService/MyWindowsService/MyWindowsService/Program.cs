6cced576846e1c96d6bac02b59b3de5a744b2412
mrcouthy <mrcouthy@gmail.com>
2014-12-29 14:29:34 +0545

    http://msdn.microsoft.com/en-us/library/system.serviceprocess.servicecontroller%28v=vs.110%29.aspx

--- WindowsService/MyWindowsService/MyWindowsService/Program.cs
+++ WindowsService/MyWindowsService/MyWindowsService/Program.cs
@@ -17,6 +17,10 @@ namespace MyWindowsService
             //http://msdn.microsoft.com/en-us/library/zt39148a%28v=vs.110%29.aspx?cs-save-lang=1&cs-lang=csharp#code-snippet-18
             //install service 
             //installutil /i C:\Users\debug\xyz.dll
+
+            //In Visual Studio, open Server Explorer (Keyboard: Ctrl+Alt+S), and access the Event Logs node for the local computer.
+
+            //Locate the listing for MyNewLog (or MyLogFile1, if you used the optional procedure to add command-line arguments) and expand it. You should see entries for the two actions (start and stop) your service has performed. 
             ServiceBase[] ServicesToRun;
             ServicesToRun = new ServiceBase[] 
             { 
