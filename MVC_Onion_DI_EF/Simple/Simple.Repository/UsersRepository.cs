c5dbaa69e2c4c2a5100de055a3303d661222205a
Dj <mrcouthy@gmail.com>
2015-01-03 12:34:52 +0545

    Adding simple model users .in domain ,repository and service

---
+++ MVC_Onion_DI_EF/Simple/Simple.Repository/UsersRepository.cs
@@ -0,0 +1,22 @@
+﻿using Simple.Interface.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple.Repository
+{
+    public class UsersRepository : IUsersRepository
+    {
+        private readonly string configuration;
+        public UsersRepository(string configuration)
+        {
+            this.configuration = configuration;
+        }
+        public Domain.Users GetUsers(string userID)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MVC_Onion_DI_EF/Simple/Simple.Service/UsersService.cs
@@ -0,0 +1,17 @@
+﻿using Simple.Interface.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple.Service
+{
+    public class UsersService : IUsersService
+    {
+        Domain.Users IUsersService.GetUsers(string userID)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
