e7acee71aaec3236de5ad55d345a9479f2adf70b
Dj <mrcouthy@gmail.com>
2015-01-03 15:28:41 +0545

    First time web works without DI

---
+++ MVC_Onion_DI_EF/Simple/Simple.Web/Controllers/UsersController.cs
@@ -0,0 +1,22 @@
+﻿using Simple.Repository;
+using Simple.Service;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Simple.Web.Controllers
+{
+    public class UsersController : Controller
+    {
+        public ActionResult Index()
+        {
+            string constr = ConfigurationManager.ConnectionStrings["SimpleConnection"].ConnectionString;
+            UsersRepository ur = new UsersRepository(constr);
+            UsersService us = new UsersService(ur);
+            return this.View(us.GetUsers());
+        }
+    }
+}
