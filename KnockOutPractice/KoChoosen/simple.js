866987cdd4e6d40a816f755e9474fa91b99ef0a9
Dj <mrcouthy@gmail.com>
2015-02-01 17:27:37 +0545

    adds

--- KnockOutPractice/KoChoosen/simple.js
+++ KnockOutPractice/KoChoosen/simple.js
@@ -1,5 +1,40 @@
 $(function () {
 
+
+
+
+
+
+
+    // Here's my data model
+    var ViewModel = function (firstName, lastName, phones) {
+        var tesphones = ['poire', 'annas', 'bannana','kiwi','avocado'];
+        var self = this;
+         
+        //arrya example
+        self.phones = ko.observableArray(phones);
+
+        self.phones(tesphones);
+        this.newPhone = ko.observable();
+
+        this.addPhone = function () {
+            if (this.newPhone() != "") {
+                this.phones.push(this.newPhone());// try this with no brackets ie this.newPhone and get mad !
+                this.newPhone("");
+            }
+            //when using observables always access members as as function ie newphone() => value not noewphone => observable
+        }
+
+        this.selectedPhone = ko.observable("");
+
+        //more complex select example
+       // var dropDown = [{ name: "dhiraj", id: "1", phone: "9841190907" }, { name: "Hero", id: "2", phone: "5536114" }];
+
+    };
+
+   
+    ko.applyBindings(new ViewModel('Dhiraj', 'Bajracharya', ['9841190907', '9841228718', '5536114', '123456', '5425654'])); // This makes Knockout get to work
+
 //Chosen
 // Chosenify every multiple select DOM elements with class 'chosen'
         $('#gogo').chosen();
@@ -35,34 +70,12 @@ $(function () {
 
 
 
-    // Here's my data model
-    var ViewModel = function (firstName, lastName, phones) {
-        var tesphones = ['poire', 'annas', 'bannana','kiwi','avocado'];
-        var self = this;
-         
-        //arrya example
-        self.phones = ko.observableArray(phones);
 
-        self.phones(tesphones);
-        this.newPhone = ko.observable();
 
-        this.addPhone = function () {
-            if (this.newPhone() != "") {
-                this.phones.push(this.newPhone());// try this with no brackets ie this.newPhone and get mad !
-                this.newPhone("");
-            }
-            //when using observables always access members as as function ie newphone() => value not noewphone => observable
-        }
 
-        this.selectedPhone = ko.observable("");
 
-        //more complex select example
-       // var dropDown = [{ name: "dhiraj", id: "1", phone: "9841190907" }, { name: "Hero", id: "2", phone: "5536114" }];
 
-    };
 
-   
-    ko.applyBindings(new ViewModel('Dhiraj', 'Bajracharya', ['9841190907', '9841228718', '5536114', '123456', '5425654'])); // This makes Knockout get to work
 });
 
 
