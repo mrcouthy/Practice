429b8a380cce23b4e9bcf11c628e57d3557a978d
mrcouthy <mrcouthy@gmail.com>
2015-02-26 15:03:13 +0545

    Moving Sql stuff to sql repo

--- SQLTools/Shema Doc.sql
+++
@@ -1,7 +0,0 @@
-SELECT
-COLUMN_NAME as Name,
-DATA_TYPE + case  when  CHARACTER_MAXIMUM_LENGTH is null then '' else '(' + ltrim(str(CHARACTER_MAXIMUM_LENGTH)) + ')' end as Type   ,
-COLUMN_DEFAULT [Default],
-IS_NULLABLE Nullable 
-from INFORMATION_SCHEMA.COLUMNS where TABLE_NAME ='RawTables_Lookup'
-
