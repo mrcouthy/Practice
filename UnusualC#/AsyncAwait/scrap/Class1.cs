ce7ed612e0dfb4fb9788b323a957dffc76244b9b
Dj <mrcouthy@gmail.com>
2015-12-15 10:44:21 +0545

    http://stackoverflow.com/questions/27032631/when-are-async-and-await-needed-in-c Async Method: Naming convention : Ends in Async async Task<string> RunSlowOperationTaskAsync()
    Till it encounters an await as in
    await Task.Delay(2000);
    It works in same thread
    
    After it encounters await , it can jump back to other threads and rest of it continues in a new thread
    And old execution sequence constitutes in same thread.
    Beautiful!

---
+++ UnusualC#/AsyncAwait/scrap/Class1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace scrap
+{
+    public class Operation
+    {
+        public string RunSlowOperation()
+        {
+            Console.WriteLine("Slow operation running on thread id {0}", Thread.CurrentThread.ManagedThreadId);
+            Thread.Sleep(2000);
+            Console.WriteLine("Slow operation about to finish on thread id {0}", Thread.CurrentThread.ManagedThreadId);
+            return "This is very slow...";
+        }
+
+        public void RunTrivialOperation()
+        {
+            string[] words = new string[] { "i", "love", "dot", "net", "four", "dot", "five" };
+            foreach (string word in words)
+            {
+                Console.WriteLine(word);
+            }
+        }
+
+        //edit
+        public Task<string> RunSlowOperationTask()
+        {
+            return Task.Factory.StartNew<string>(RunSlowOperation);
+        }
+
+        public async Task<string> RunSlowOperationTaskAsync()
+        {
+            Console.WriteLine("Slow operation running on thread id {0}", Thread.CurrentThread.ManagedThreadId);
+            await Task.Delay(2000);
+            Console.WriteLine("Slow operation about to finish on thread id {0}", Thread.CurrentThread.ManagedThreadId);
+            return "This is very slow...";
+        }
+    }
+}
---
+++ UnusualC#/AsyncAwait/scrap/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace scrap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Operation operation = new Operation();
+
+            //string result = operation.RunSlowOperation();
+            //Task<string> result = operation.RunSlowOperationTask();
+            Task<string> result = operation.RunSlowOperationTaskAsync();
+
+            operation.RunTrivialOperation();
+
+            //Console.WriteLine("Return value of slow operation: {0}", result);
+            Console.WriteLine("Return value of slow operation: {0}", result.Result);
+            Console.WriteLine("The main thread has run complete on thread number {0}", Thread.CurrentThread.ManagedThreadId);
+            Console.ReadLine();
+
+          
+
+           
+
+           
+
+        }
+    }
+}
