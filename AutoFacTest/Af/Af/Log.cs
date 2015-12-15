1470a663a429e7ba369bf0cf004d4df4f2c02d7b
Dj <mrcouthy@gmail.com>
2015-12-15 15:13:56 +0545

    Adding Af test

---
+++ AutoFacTest/Af/Af/Log.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Af
+{
+    public interface ILog
+    {
+        void Write();
+    }
+
+    public class Log:ILog
+    {
+        public void Write()
+        {
+            Console.Write("write");
+        }
+    }
+
+    public interface IContext
+    {
+        void DataBase();
+    }
+
+    public class Context : IContext
+    {
+      public  ILog Log { get; set; }
+        public void DataBase()
+        {
+            Log.Write();
+            Console.Write("database");
+        }
+    }
+
+}
---
+++ AutoFacTest/Af/Af/Program.cs
@@ -0,0 +1,26 @@
+﻿using Autofac;
+using System;
+using System.Reflection;
+
+namespace Af
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterAssemblyTypes(Assembly.GetExecutingAssembly()).AsImplementedInterfaces().PropertiesAutowired();
+
+            //builder.RegisterType<Log>().As<ILog>().InstancePerDependency().PropertiesAutowired();
+            IContainer Container = builder.Build();
+            using (var scope = Container.BeginLifetimeScope())
+            {
+                var contexter = scope.Resolve<IContext>();
+                contexter.DataBase();
+                //configReader.ConfigString();
+            }
+            Console.ReadLine();
+        }
+    }
+}
