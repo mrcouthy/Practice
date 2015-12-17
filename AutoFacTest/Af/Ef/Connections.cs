39de13d273fcdfbd86c59033a928e50df7ad263a
Dj <mrcouthy@gmail.com>
2015-12-17 15:11:09 +0545

    Adding Ef

---
+++ AutoFacTest/Af/Ef/Connections.cs
@@ -0,0 +1,7 @@
+﻿namespace Ef
+{
+    public class Connections
+    {
+        public static readonly string ConnectionString="Server=s1;initial catalog=EfTest;user=abc;";
+    }
+}
---
+++ AutoFacTest/Af/Ef/Model.cs
@@ -0,0 +1,24 @@
+﻿using System.Data.Entity;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity.Infrastructure;
+using Ef;
+
+namespace MigrationsDemo
+{
+    public class BlogContext : DbContext
+    {
+        public BlogContext():base(Connections.ConnectionString)
+        {
+
+        }
+        
+        public DbSet<Blog> Blogs { get; set; }
+    }
+
+    public class Blog
+    {
+        public int BlogId { get; set; }
+        public string Name { get; set; }
+    }
+}
